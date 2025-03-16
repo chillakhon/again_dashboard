c33fa882919caaf7c47c565adc654a7b8dfebf82
dev_islam <islamnepesov27@gmail.com>
2025-03-17 02:38:26 +0500

    api list added

---
+++ src/api/warehousesApi.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+
+class warehousesApi{
+
+    //Materials Api 
+    
+    getMaterials() {
+        return axios.get("/materials");
+    }
+
+    getMaterial(id) {
+        return axios.get("/materials/" + id);
+    }
+    
+    createMaterial(data) {
+        return axios.post("/materials", data);
+    }
+
+    updateMaterial(id, data) {
+        return axios.put("/materials/" + id, data);
+    }
+
+    deleteMaterial(id) {
+        return axios.delete("/materials/" + id);
+    }
+
+    //Recipes Api 
+    
+    getRecipes() {
+        return axios.get("/recipes");
+    }
+
+    getRecipe(id) {
+        return axios.get("/recipes/" + id);
+    }
+    
+    createRecipe(data) {
+        return axios.post("/recipes", data);
+    }
+
+    updateRecipe(id, data) {
+        return axios.put("/recipes/" + id, data);
+    }
+
+    deleteRecipe(id) {
+        return axios.delete("/recipes/" + id);
+    }
+    
+}
+
+export default new warehousesApi();
