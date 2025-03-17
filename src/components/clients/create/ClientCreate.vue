b58df6f77002d214fdbd7cb0505064e9c417782d
dev_islam <islamnepesov27@gmail.com>
2025-03-17 16:28:02 +0500

    client create form

--- src/components/clients/create/ClientCreate.vue
+++ src/components/clients/create/ClientCreate.vue
@@ -18,7 +18,6 @@
                                 <input v-model="data.last_name" :disabled="isLoading" id="last_name" name="last_name" type="text" autocomplete="last_name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:text-sm/6" />
                             </div>
                         </div>
-
                     </div>
                     <div class="mt-8">
                         <label for="email" class="block text-sm/6 font-medium text-gray-900">Email</label>
