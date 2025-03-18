43277dfe25533974e5fed7b3e73f6a159630c5ee
chillakhon <shoev-03@mail.ru>
2025-03-19 01:12:26 +0500

    321

--- src/components/auth/SignIn.vue
+++ src/components/auth/SignIn.vue
@@ -1,53 +1,55 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <img class="mx-auto h-10 w-auto" :src='logo' alt="Again" />
-            <h1 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Dashboard</h1>
-            <h3 class="mt-6 text-center text-xl font-bold tracking-tight text-gray-900">Sign in to your account</h3>
-        </div>
-  
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-            <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-                <form @submit.prevent="handleSubmit" class="space-y-6">
-                    <div>
-                        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
-                        <div class="mt-2">
-                            <input v-model="form.email" :disabled="isLoading" type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:text-sm/6" />
-                        </div>
-                    </div>
-        
-                    <div>
-                        <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-                        <div class="mt-2">
-                            <input v-model="form.password" :disabled="isLoading"  type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:text-sm/6" />
-                        </div>
-                    </div>
+  <div class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <img class="mx-auto h-10 w-auto" :src='logo' alt="Again"/>
+      <h1 class="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Dashboard</h1>
+      <h3 class="mt-6 text-center text-xl font-bold tracking-tight text-gray-900">Sign in to your account</h3>
+    </div>
 
-                    <div v-if="hasError">
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+      <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+        <form @submit.prevent="handleSubmit" class="space-y-6">
+          <div>
+            <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+            <div class="mt-2">
+              <input v-model="form.email" :disabled="isLoading" type="email" name="email" id="email"
+                     autocomplete="email" required=""
+                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:text-sm/6"/>
+            </div>
+          </div>
+
+          <div>
+            <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+            <div class="mt-2">
+              <input v-model="form.password" :disabled="isLoading" type="password" name="password" id="password"
+                     autocomplete="current-password" required=""
+                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:text-sm/6"/>
+            </div>
+          </div>
+
+          <div v-if="hasError">
                         <span class="text-red-600 font-semibold capitalize">
                             Произошла ошибка
                         </span>
-                    </div>
+          </div>
+          <div>
+            <button type="submit" :disabled="isLoading"
+                    class="flex w-full justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">
+              <span v-if="!isLoading">Sign in</span>
+              <span v-else>Loading...</span>
+            </button>
+          </div>
 
-        
-                    <div>
-                        <button type="submit" :disabled="isLoading" class="flex w-full justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">
-                            <span v-if="!isLoading">Sign in</span>
-                            <span v-else>Loading...</span>
-                        </button>
-                    </div>
-
-                </form>
-            </div>
-        </div>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, reactive, computed, watch } from 'vue';
-import { useStore } from 'vuex';
-import { useRouter } from 'vue-router';
-import { PhEye, PhEyeSlash } from '@phosphor-icons/vue';
+import {computed, reactive, ref, watch} from 'vue';
+import {useStore} from 'vuex';
+import {useRouter} from 'vue-router';
 
 const store = useStore()
 const router = useRouter()
@@ -55,8 +57,8 @@ const router = useRouter()
 const logo = '/logo.svg';
 
 const form = reactive({
-    name: '',
-    password: '',
+  name: '',
+  password: '',
 })
 
 const signIn = (form) => store.dispatch('auth/signIn', form)
@@ -66,18 +68,18 @@ const hasError = computed(() => store.getters['auth/hasError'])
 const isLoading = computed(() => store.getters['auth/isLoading'])
 
 watch(authenticated, (newValue) => {
-    if (newValue) {
-        router.push('/dashboard')
-    }
+  if (newValue) {
+    router.push('/dashboard')
+  }
 })
 
 const handleSubmit = () => {
-    signIn(form)
+  signIn(form)
 }
 
 const passwordVisible = ref(false);
 const togglePasswordVisibility = () => {
-    passwordVisible.value = !passwordVisible.value;
+  passwordVisible.value = !passwordVisible.value;
 };
 
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import axiosPlugin from './plugins/axios';
+
 import store from './store'
-import axios from 'axios'
-import PluginAxios from './plugins/axios.js'
 import Cookies from 'js-cookie'
 
 import './assets/css/tailwind.css'
@@ -14,9 +14,10 @@ require('./store/modules/auth/subscriber.js');
 
 const access_token = Cookies.get('access_token');
 
-store.dispatch( 'auth/attempt', access_token ).then(() => {
+store.dispatch('auth/attempt', access_token).then(() => {
     createApp(App)
         .use(store)
         .use(router)
+        .use(axiosPlugin)
         .mount('#app')
 })
