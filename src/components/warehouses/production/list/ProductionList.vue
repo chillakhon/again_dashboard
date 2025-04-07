ec6a5e3a37cecce69189ac8a141dbfe20e018d8d
chillakhon <shoev-03@mail.ru>
2025-04-07 06:49:37 +0500

    321

--- src/components/warehouses/production/list/ProductionList.vue
+++ src/components/warehouses/production/list/ProductionList.vue
@@ -2,10 +2,10 @@
   <div class="container mx-auto px-4 py-6">
     <div class="flex justify-between items-center mb-6">
       <h1 class="text-2xl font-bold text-gray-800">Производственные задания</h1>
-      <RouterLink to="/warehouses/recipes/create">
+      <RouterLink to="/warehouses/production/create">
         <Button class="gap-2">
           <PlusIcon class="h-4 w-4" />
-          Создать техкарту
+          Создать задания
         </Button>
       </RouterLink>
     </div>
