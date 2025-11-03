7e0bc2775a0f3214e9450d42e4a763851dc9092f
chillakhon <shoev-03@mail.ru>
2025-11-03 14:35:17 +0500

    latest

--- src/echo.js
+++ src/echo.js
@@ -1,42 +1,21 @@
-// src/echo.js - Упрощенная версия
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 import Cookies from "js-cookie";
 
 window.Pusher = Pusher;
 
-// Environment variables
 const REVERB_KEY = process.env.VUE_APP_REVERB_APP_KEY || '';
 const REVERB_HOST = process.env.VUE_APP_REVERB_HOST || window.location.hostname;
-const REVERB_PORT = Number(process.env.VUE_APP_REVERB_PORT || 8002);
-const REVERB_SCHEME = process.env.VUE_APP_REVERB_SCHEME || (location.protocol === 'https:' ? 'https' : 'http');
+const REVERB_PORT = Number(process.env.VUE_APP_REVERB_PORT || 443);
+const REVERB_SCHEME = process.env.VUE_APP_REVERB_SCHEME || 'wss';
 const API_BASE = (process.env.VUE_APP_API_BASE_URL || '').replace(/\/$/, '');
 
 const authEndpoint = API_BASE ? `${API_BASE}/broadcasting/auth` : '/broadcasting/auth';
-const useTLS = REVERB_SCHEME === 'https';
 const access_token = Cookies.get('access_token');
 
-
-
-//local params
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: REVERB_KEY,
-//     cluster: 'mt1',
-//     wsHost: REVERB_HOST,
-//     wsPort: REVERB_PORT,
-//     wssPort: REVERB_PORT,
-//     forceTLS: false,
-//     encrypted: false,
-//     enabledTransports: ['ws'],
-//     disableStats: true,
-//     authEndpoint: authEndpoint,
-//     auth: {
-//         headers: {
-//             'Authorization': `Bearer ${access_token}`,
-//         },
-//     },
-// });
+// Определяем транспорты в зависимости от схемы
+const enabledTransports = REVERB_SCHEME === 'wss' ? ['wss'] : ['ws'];
+const forceTLS = REVERB_SCHEME === 'wss';
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
@@ -45,9 +24,9 @@ window.Echo = new Echo({
     wsHost: REVERB_HOST,
     wsPort: REVERB_PORT,
     wssPort: REVERB_PORT,
-    forceTLS: true,
-    encrypted: true,
-    enabledTransports: ['ws','wss'],
+    forceTLS: forceTLS,
+    encrypted: forceTLS,
+    enabledTransports: enabledTransports,
     disableStats: true,
     authEndpoint: authEndpoint,
     auth: {
@@ -58,4 +37,5 @@ window.Echo = new Echo({
 });
 
 console.log('Echo instance created:', window.Echo);
+
 export default window.Echo;
