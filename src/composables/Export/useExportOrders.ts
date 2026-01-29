713080347cd01267c9a1014bd420a5eb7c208f42
chillakhon <shoev-03@mail.ru>
2026-01-29 11:11:28 +0500

    latest

--- src/composables/Export/useExportOrders.ts
+++ src/composables/Export/useExportOrders.ts
@@ -35,10 +35,9 @@ export function useExportOrders() {
                 let fileName = `orders_${Date.now()}.csv`
 
                 if (contentDisposition) {
-                    const fileNameMatch = contentDisposition.match(/filename="?(.+)"?/)
-                    if (fileNameMatch && fileNameMatch[1]) {
-                        fileName = fileNameMatch[1]
-                    }
+                    const fileNameMatch = contentDisposition.match(/filename="([^"]+)"/i)
+                    if (fileNameMatch?.[1]) fileName = fileNameMatch[1]
+
                 }
 
                 link.setAttribute('download', fileName)
